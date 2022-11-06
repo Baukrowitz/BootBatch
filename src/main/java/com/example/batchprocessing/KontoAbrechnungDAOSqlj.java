ebd6b41da9dcecf90c193c5e2c0179a52359e17d
Administrator <Administrator@172.31.32.96>
2022-11-06 12:21:58 +0000

    sqlj
    Signed-off-by: Administrator <Administrator@172.31.32.96>

--- src/main/java/com/example/batchprocessing/KontoAbrechnungDAOSqlj.java
+++ src/main/java/com/example/batchprocessing/KontoAbrechnungDAOSqlj.java
@@ -1,5 +1,29 @@
 package com.example.batchprocessing;
 
-public class KontoAbrechnungDAOSqlj {
+import java.util.List;
+
+
+public class KontoAbrechnungDAOSqlj implements KontoAbrechnungDAO {
+
+    
+	
+	@Override
+	public List<KontoAbrechnung> alleKontoAbrechnung() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertGevo(Gevo gevo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteGevo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
+ 
